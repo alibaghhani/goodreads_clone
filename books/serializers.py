28a01e9b8f50226a1ebfc1474df9caee41b71f80
alibaghani <baghaniali2006@gmail.com>
2024-09-28 16:46:33 +0330

    feat(new serializer): Add new serializer

--- books/serializers.py
+++ books/serializers.py
@@ -1,9 +1,16 @@
-
 from rest_framework.serializers import ModelSerializer
-from books.models import Book
+from books.models import Book, Genre
 
 
 class BookSerializer(ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'title', 'author', 'description')
+
+
+class GenreSerializer(ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Genre
+        fields = ('id', 'name')
