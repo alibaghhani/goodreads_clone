a5b1aad62299069f2d5c5256069f62b7581ecbbc
alibaghani <baghaniali2006@gmail.com>
2024-09-28 13:58:42 +0330

    style(reformat file): reformat the file and spaces

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,3 @@
-
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,7 +7,6 @@ class User(AbstractUser):
     username = models.CharField(max_length=250, unique=True, null=True, blank=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['admin_name']
