c2564196eff8403d15400d764ffbbd267d51247c
alibaghani <baghaniali2006@gmail.com>
2024-09-28 13:59:35 +0330

    chore(new endpoint): verify jwt token endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from .views import RegisterUserViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,
+    TokenRefreshView, TokenVerifyView,
 )
 
 
@@ -19,6 +19,7 @@ router.register(
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ]
 urlpatterns += router.urls
