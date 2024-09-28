c5e45f8ef3aa95c9210ae1f9c98bf7a5d2d740ee
alibaghani <baghaniali2006@gmail.com>
2024-09-28 16:45:58 +0330

    feat(new model): Add new model named genre

--- books/models.py
+++ books/models.py
@@ -7,3 +7,15 @@ class Book(TimeStampMixin):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     description = models.TextField()
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(TimeStampMixin):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
