5547cdbab2e7f855206f65ed605c5c3c6719a2ed
alibaghani <baghaniali2006@gmail.com>
2024-09-28 13:21:11 +0330

    chore(seetings.py): Add AUTH_USER MODEL and REST_FRAMEWORK settings to settings.py

--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
