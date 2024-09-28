6f64db3324e359cc6913fd50200cf65c5fe81c31
alibaghani <baghaniali2006@gmail.com>
2024-09-28 12:52:28 +0330

    chore(User model ViewSet): Implement ViewSet for user model

---
+++ authentication/views.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from .serializers import UserSerializer
+from authentication.models import User
+
+
+class RegisterUserViewSet(ViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'message': 'invalid form'}, status=status.HTTP_403_FORBIDDEN)
+
+
+
+
+
