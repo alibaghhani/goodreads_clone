4e55729a3abf845ee176964ee3719cc2f46aafba
alibaghani <baghaniali2006@gmail.com>
2024-09-28 12:53:56 +0330

    chore(Create router): Create router for UserViewSet

---
+++ authentication/urls.py
@@ -0,0 +1,24 @@
+
+
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import RegisterUserViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+router = DefaultRouter()
+router.register(
+    r'users',
+    RegisterUserViewSet,
+    basename='register'
+)
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
+urlpatterns += router.urls
