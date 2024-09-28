dc52910f833a37b284cf5484585b89be04706691
alibaghani <baghaniali2006@gmail.com>
2024-09-28 16:51:09 +0330

    feat: create a new viewset for genre model

--- books/views.py
+++ books/views.py
@@ -1,14 +1,41 @@
-
-
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from books.models import Book
-from books.serializers import BookSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from books.models import Book, Genre
+from books.serializers import BookSerializer, GenreSerializer
 
 
 class BooksViewSet(ViewSet):
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAdminUser()]
+        elif self.action == 'list':
+            return [IsAuthenticated()]
+        return super().get_permissions()
+
+    authentication_classes = [JWTAuthentication]
+
     def list(self, request):
         queryset = Book.objects.all()
         serializer = BookSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class GenreViewSet(ViewSet):
+    permission_classes = [AllowAny]
+
+    def list(self):
+        queryset = Genre.objects.all()
+        serializer = GenreSerializer(queryset, many=True)
         return Response(serializer.data)
